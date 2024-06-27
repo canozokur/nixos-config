2dddc5260ba0985a5ec8ea6d8bc4d3980e6f5d8b
canozokur <13416785+canozokur@users.noreply.github.com>
2024-06-27 12:10:04 +0300

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = 5;
+    };
+    initrd = {
+      luks.devices = {
+        luksroot = {
+          device = "/dev/disk/by-uuid/12e67b58-0910-4694-8b82-de18bb7d059c";
+        };
+      };
+      availableKernelModules = [
+        "aesni_intel"
+        "cryptd"
+      ];
+    };
+  };
+
+  networking.hostName = "virtnixbox";
+  time.timeZone = "Europe/Helsinki";
+
+  users.users.canozokur = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    createHome = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+  ];
+
+  services.openssh.enable = true;
+  system.stateVersion = "23.11";
+
+}
+
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.virtnixbox = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./virtnixbox/hardware-configuration.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
