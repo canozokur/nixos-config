aad6bc2b96a8d0eb93053b20c6a776ba272b1a28
canozokur <13416785+canozokur@users.noreply.github.com>
2024-07-17 12:53:08 +0300

    Create an attrSet for nixosConfs

--- lib/mkbox.nix
+++ lib/mkbox.nix
@@ -1,13 +1,11 @@
 { nixpkgs }:
-{ boxes }:
-let
-  mkSingleBox = box: system:
-    nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ../boxes/${box}/hardware-configuration.nix
-        ../boxes/${box}/configuration.nix
-      ];
-    };
-in
-builtins.mapAttrs (name: value: mkSingleBox name value.system) boxes
+{ box, system }:
+{
+  ${box} = nixpkgs.lib.nixosSystem {
+    inherit system;
+    modules = [
+      ../boxes/${box}/hardware-configuration.nix
+      ../boxes/${box}/configuration.nix
+    ];
+  };
+}
