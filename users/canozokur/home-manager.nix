d25950024fa3e745de422046c1d7f3b33e1c3d9e
canozokur <13416785+canozokur@users.noreply.github.com>
2024-07-23 15:36:11 +0300

    Add git config

--- users/canozokur/home-manager.nix
+++ users/canozokur/home-manager.nix
@@ -9,4 +9,29 @@
     ripgrep
     watch
   ];
+
+  programs.git = {
+    enable = true;
+    userEmail = "13416785+canozokur@users.noreply.github.com";
+    userName = "canozokur";
+    extraConfig = {
+      pull = { ff = "only"; };
+      init = { defaultbranch = "main"; };
+      push = { autosetupremote = true; };
+      url."git@github.com:".insteadOf = "https://github.com/";
+    };
+    delta = {
+      enable = true;
+      options = {
+        features = "side-by-side line-numbers decorations";
+        whitespace-error-style = "22 reverse";
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-style = "bold yellow ul";
+          file-decoration-style = "none";
+        };
+      };
+    }; 
+  };
+    
 }
