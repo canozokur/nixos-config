388da7b4766d0efef519a2ceac1875a931464787
canozokur <13416785+canozokur@users.noreply.github.com>
2024-09-03 17:18:27 +0300

    Add nixvim to inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,23 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: 
-  let
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
     mkBox = import ./lib/mkbox.nix {
       inherit nixpkgs;
       inherit home-manager;
