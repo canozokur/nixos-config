9852e1658d4c4417bfb17e525047417dc2471952
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-02 14:35:04 +0300

    Update barbar keymaps

--- users/canozokur/nixvim/plugins/barbar.nix
+++ users/canozokur/nixvim/plugins/barbar.nix
@@ -3,9 +3,14 @@
   programs.nixvim.plugins = {
     barbar = {
       enable = true;
+      keymaps = {
+        pick.key = "<C-p>";
+        pickDelete.key = "<C-s-p>";
+      };
     };
 
-    # barbar depends on web-devicons
+    # barbar depends on web-devicons and gitsigns
     web-devicons.enable = true;
+    gitsigns.enable = true;
   };
 }
