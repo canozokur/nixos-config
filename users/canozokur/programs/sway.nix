985f93c916792066c3f3a2ef65a1315a174a0c4b
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-07 23:09:06 +0300

    Override ghostty config so we can run it in vmware

--- users/canozokur/programs/sway.nix
+++ users/canozokur/programs/sway.nix
@@ -1,10 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.gnome-keyring.enable = true; # enable a keyring provider
   wayland.windowManager.sway = {
     enable = true;
+    xwayland = true;
     config = {
       modifier = "Mod4";
+      terminal = "${pkgs.ghostty}/bin/ghostty";
     };
   };
 }
