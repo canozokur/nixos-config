d3790637f6e2faae3b0ac696741f85ff1c521070
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-08 14:47:28 +0300

    Add clipboard manager to sway

--- users/canozokur/programs/sway.nix
+++ users/canozokur/programs/sway.nix
@@ -4,10 +4,27 @@
   wayland.windowManager.sway = {
     enable = true;
     xwayland = true;
+    wrapperFeatures.gtk = true;
     config = {
       modifier = "Mod4";
       terminal = "${pkgs.ghostty}/bin/ghostty";
+      fonts = {
+        names = ["Droid Sans"];
+        style = "Regular";
+        size = "8";
+      };
+
+      startup = [
+        { command = "${pkgs.wl-clipboard}/bin/wl-paste --primary --watch wl-copy"; always = true; }
+      ];
     };
+    extraConfig = ''
+      default_border pixel 10
+    '';
   };
+
+  home.packages = [
+    pkgs.wl-clipboard
+  ];
 }
 
