4378ddee3209c344f4861e2d54a2c00aa9d3f7f8
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-17 02:08:46 +0300

    Add spotify-player

--- users/canozokur/programs/spotify-player.nix
+++ users/canozokur/programs/spotify-player.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
+  sops.secrets."spotify/client-id" = {};
+
   programs.spotify-player = {
     enable = true;
     settings = {
       theme = "catppuccin_mocha";
+      client_id_command = {
+        command = "cat";
+        args = [ config.sops.secrets."spotify/client-id".path ];
+      };
     };
   };
 }
