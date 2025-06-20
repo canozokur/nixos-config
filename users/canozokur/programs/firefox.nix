fdff76529d40466cde8870ad00df80e1f2dc98c0
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-20 22:23:06 +0300

    Add search for hm options

--- users/canozokur/programs/firefox.nix
+++ users/canozokur/programs/firefox.nix
@@ -30,12 +30,12 @@
                   { name = "query"; value = "{searchTerms}"; }
                 ];
               }];
-              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
               definedAliases = [ "@np" ];
             };
             "NixOS Wiki" = {
               urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-              icon = "https://nixos.wiki/favicon.png";
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = [ "@nw" ];
             };
@@ -44,6 +44,12 @@
               icon = "https://kagi.com/asset/9ade212/kagi_assets/logos/blue_1.svg?v=0679f32d13823ea4193af65d0d67c6e78f17e0da";
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = [ "@k" ];
+            };
+            "Home Manager Options" = {
+              urls = [{ template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master"; }];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = [ "@hmo" ];
             };
             "Noogle" = {
               urls = [{ template = "https://noogle.dev/q?term={searchTerms}"; }];
