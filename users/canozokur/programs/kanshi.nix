10206cad2cb8234af066e462a24046b238456a78
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-25 01:10:54 +0300

    Fix workspace assignments on dual monitors

--- users/canozokur/programs/kanshi.nix
+++ users/canozokur/programs/kanshi.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.kanshi = {
     enable = true;
@@ -29,6 +29,20 @@
               mode = "2560x1440@60Hz";
               position = "1440,0";
             }
+          ];
+          exec = [
+            "${pkgs.sway}/bin/swaymsg workspace 1, workspace 1 output '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 2, workspace 2 output '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 3, workspace 3 output '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 4, workspace 4 output '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 5, workspace 5 output '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 6, workspace 6 output '\"ASUSTek COMPUTER INC VG27B LALMQS275717\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 7, workspace 7 output '\"ASUSTek COMPUTER INC VG27B LALMQS275717\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 8, workspace 8 output '\"ASUSTek COMPUTER INC VG27B LALMQS275717\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 9, workspace 9 output '\"ASUSTek COMPUTER INC VG27B LALMQS275717\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 1, move workspace to '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 2, move workspace to '\"ASUSTek COMPUTER INC PG279QE K7LMQS096556\"'"
+            "${pkgs.sway}/bin/swaymsg workspace 6, move workspace to '\"ASUSTek COMPUTER INC VG27B LALMQS275717\"'"
           ];
         };
       }
