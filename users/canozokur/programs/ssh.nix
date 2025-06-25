6b6791a067c4db4de770634b3b857aee65031e74
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-25 14:58:54 +0300

    Keep ssh conns alive for 30s

--- users/canozokur/programs/ssh.nix
+++ users/canozokur/programs/ssh.nix
@@ -3,6 +3,7 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
+    serverAliveInterval = 10;
     extraConfig = "SetEnv TERM=xterm";
     matchBlocks = nix-secrets.ssh.matchBlocks;
   };
