e25128f27d32f251caf4192f6745bab486b083fe
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-26 16:14:12 +0300

    Try undervolt again with lower defaults

--- boxes/nexusbox/undervolt.nix
+++ boxes/nexusbox/undervolt.nix
@@ -1,9 +1,10 @@
 { ... }:
+# old config
+# undervolt -v --turbo 1 -p1 35 10 -p2 35 10
 {
   services.undervolt = {
     enable = true;
-    coreOffset = -75;
-    temp = 70;
+    coreOffset = -50;
     verbose = true;
   };
 }
