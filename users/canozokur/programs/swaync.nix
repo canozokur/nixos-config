5bd12593da53277b0b00aad71a28d726c4313d27
canozokur <13416785+canozokur@users.noreply.github.com>
2025-06-29 21:22:14 +0300

    Add swaync with ctp-mocha colors

--- users/canozokur/programs/swaync.nix
+++ users/canozokur/programs/swaync.nix
@@ -1,6 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  ctp-mocha = pkgs.fetchurl {
+    url = "https://github.com/catppuccin/swaync/releases/download/v0.2.3/mocha.css";
+    hash = "sha256-Hie/vDt15nGCy4XWERGy1tUIecROw17GOoasT97kIfc=";
+  };
+
+  # by default ctp-mocha is configured for Ubuntu fonts
+  # we don't want that
+  ctp-mocha-fc = builtins.replaceStrings
+    [ "font-family: \"Ubuntu Nerd Font\";" ]
+    [ "font-family: \"CaskaydiaCove NFM\";" ]
+    (builtins.readFile ctp-mocha);
+in
 {
+  # this is required for proper icons i.e. "no notifications" icon
+  home.packages = [ pkgs.adwaita-icon-theme ];
+
   services.swaync = {
     enable = true;
+    settings = {
+    };
+    style = ctp-mocha-fc;
   };
 }
