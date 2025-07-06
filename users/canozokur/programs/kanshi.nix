5af2f24d82decd09f43d7258487bc9ed0f0a17fd
canozokur <13416785+canozokur@users.noreply.github.com>
2025-07-07 00:29:31 +0300

    Only add exec commands on sway

--- users/canozokur/programs/kanshi.nix
+++ users/canozokur/programs/kanshi.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 let
   swayMsg = "${pkgs.sway}/bin/swaymsg";
   assignWorkspace = ws: monitor: "${swayMsg} workspace ${toString ws}, workspace ${toString ws} output '\"${monitor}\"'";
@@ -35,10 +35,10 @@ in
               position = "1440,0";
             }
           ];
-          exec = builtins.map (ws: assignWorkspace ws "ASUSTek COMPUTER INC PG279QE K7LMQS096556") (lib.range 1 5)
+          exec = lib.mkIf config.wayland.windowManager.sway.enable (builtins.map (ws: assignWorkspace ws "ASUSTek COMPUTER INC PG279QE K7LMQS096556") (lib.range 1 5)
             ++ builtins.map (ws: assignWorkspace ws "ASUSTek COMPUTER INC VG27B LALMQS275717") (lib.range 6 9)
             ++ builtins.map (ws: moveWorkspace ws "ASUSTek COMPUTER INC PG279QE K7LMQS096556") (lib.range 1 5)
-            ++ builtins.map (ws: moveWorkspace ws "ASUSTek COMPUTER INC VG27B LALMQS275717") (lib.range 6 9);
+            ++ builtins.map (ws: moveWorkspace ws "ASUSTek COMPUTER INC VG27B LALMQS275717") (lib.range 6 9));
         };
       }
       {
