a3e9e5e896a4c5dad36718435d9c4ff719246c19
canozokur <13416785+canozokur@users.noreply.github.com>
2025-07-24 15:06:28 +0300

    Fix undercurls in nvim

--- users/canozokur/nixvim/colorscheme.nix
+++ users/canozokur/nixvim/colorscheme.nix
@@ -8,6 +8,17 @@
         flavour = "mocha";
         transparent_background = true;
         dim_inactive.enabled = false;
+        integrations = {
+          native_lsp = {
+            enabled = true;
+            underlines = {
+              errors = ["undercurl"];
+              hints = ["undercurl"];
+              warnings = ["undercurl"];
+              information = ["undercurl"];
+            };
+          };
+        };
       };
     };
   };
