0cdd2b9092e4ebbce7365e9f27cfddd77dc1d86a
canozokur <13416785+canozokur@users.noreply.github.com>
2025-08-11 12:12:34 +0300

    Prevent login spam

--- boxes/_shared/default.nix
+++ boxes/_shared/default.nix
@@ -60,6 +60,20 @@ in
     };
   };
 
+  # taken from https://www.old.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  # these disables the log dumps on screen
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
+
   boot.consoleLogLevel = 0;
 
   security.polkit.enable = true; # required for sway
