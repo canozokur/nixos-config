35ec1dc37ededff8aff50b777cbecfd6ef99b5d3
canozokur <13416785+canozokur@users.noreply.github.com>
2025-09-01 15:25:35 +0300

    Remove auto install and use nixpkgs grammars for treesitter

--- users/canozokur/nixvim/plugins/treesitter.nix
+++ users/canozokur/nixvim/plugins/treesitter.nix
@@ -4,18 +4,7 @@
     treesitter = {
       enable = true;
       settings = {
-        auto_install = true;
-        ensure_installed = [
-          "go"
-          "python"
-          "javascript"
-          "typescript"
-          "c"
-          "lua"
-          "vim"
-          "vimdoc"
-          "query"
-        ];
+        auto_install = false;
         highlight = { enable = true; };
       };
     };
