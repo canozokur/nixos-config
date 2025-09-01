ed33fe9f9697747d85ddd1ebe2d56fa63fc4102a
canozokur <13416785+canozokur@users.noreply.github.com>
2025-09-01 12:01:32 +0300

    Disable wlan power saving and increase? power

--- boxes/nexusbox/power.nix
+++ boxes/nexusbox/power.nix
@@ -4,4 +4,10 @@
     enable = true;
     criticalPowerAction = "Hibernate";
   };
+
+  # power_save=true actually disables power saving config for some reason
+  # and power_level=5 should max it out (hopefully?)
+  boot.extraModprobeConfig = ''
+    options iwlwifi power_save=true power_level=5
+  '';
 }
