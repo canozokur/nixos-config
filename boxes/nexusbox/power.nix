6a899bf30eab9782360dbd8274ae27373813b01d
canozokur <13416785+canozokur@users.noreply.github.com>
2025-09-01 16:19:42 +0300

    Add power scheme config, see https://wiki.debian.org/iwlwifi

--- boxes/nexusbox/power.nix
+++ boxes/nexusbox/power.nix
@@ -7,7 +7,9 @@
 
   # power_save=true actually disables power saving config for some reason
   # and power_level=5 should max it out (hopefully?)
+  # for power_scheme explanation see:
+  # https://wiki.debian.org/iwlwifi#WiFi interruptions on Intel AX
   boot.extraModprobeConfig = ''
-    options iwlwifi power_save=true power_level=5
+    options iwlwifi power_save=true power_level=5 power_scheme=1
   '';
 }
