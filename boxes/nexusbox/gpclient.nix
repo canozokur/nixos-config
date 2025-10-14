eed32be63b20ace8214a6cec1972deec6077c615
canozokur <13416785+canozokur@users.noreply.github.com>
2025-10-14 19:24:53 +0300

    Update secrets and add shell script for VPN

--- boxes/nexusbox/gpclient.nix
+++ boxes/nexusbox/gpclient.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  gpclient-connect = pkgs.writeScriptBin "gpclient-connect" ''
+    if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+    fi
+    ${pkgs.gpclient}/bin/gpclient --fix-openssl connect ${inputs.nix-secrets.network.office-vpn-address}
+  '';
+in
 {
   environment.systemPackages = with pkgs; [
     gpauth
     gpclient
+    gpclient-connect
   ];
+
 }
