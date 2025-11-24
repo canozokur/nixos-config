fc14f3a3f086aafc06ca2cef2a58e911be7cbe14
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-24 16:09:35 +0200

    Move desktop configuration into profile

--- users/canozokur/profiles/desktop.nix
+++ users/canozokur/profiles/desktop.nix
@@ -1,3 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  imports = [
+    ../programs/ghostty.nix
+    ../programs/fonts.nix
+    ../programs/firefox.nix
+    ../programs/rofi.nix
+    ../programs/cliphist.nix
+    ../programs/wlsunset.nix
+    ../programs/keybase.nix
+    ../programs/kanshi.nix
+    ../programs/wezterm.nix
+    ../programs/swaync.nix
+    ../programs/hyprland.nix
+    ../programs/waybar
+    ../programs/mouse-cursor.nix
+    ../programs/clipboard.nix
+    ../programs/wpaperd.nix
+    ../programs/playerctld.nix
+    ../programs/hyprlock.nix
+    ../programs/xdg-portal.nix
+    ../programs/udiskie.nix
+  ];
+  home.packages = with pkgs; [
+    obsidian
+    spotify
+    # slurp, grim and satty are required for screenshots
+    slurp
+    grim
+    satty
+    obs-studio
+    cameractrls-gtk4
+  ];
+
+  # enable bluetooth by default
+  services.blueman-applet.enable = true;
 }
--- users/canozokur/programs/default.nix
+++ users/canozokur/programs/default.nix
@@ -3,36 +3,17 @@
   imports = [
     ./shell-config.nix
     ./git.nix
-    ./ghostty.nix
-    ./fonts.nix
-    ./firefox.nix
     ./zellij.nix
-    ./rofi.nix
-    ./cliphist.nix
-    ./wlsunset.nix
     ./ssh-agent.nix
-    ./keybase.nix
     ./ssh.nix
     ./direnv.nix
     ./google-cloud-sdk.nix
-    ./kanshi.nix
     ./nix.nix
-    ./swaync.nix
-    ./wezterm.nix
-    ./hyprland.nix
-    ./waybar
-    ./mouse-cursor.nix
-    ./clipboard.nix
-    ./wpaperd.nix
-    ./playerctld.nix
-    ./hyprlock.nix
-    ./xdg-portal.nix
     ./aider.nix
     ./lazygit.nix
     ./vault.nix
     ./qemu.nix
     ./yazi.nix
-    ./udiskie.nix
   ];
 
   home.packages = with pkgs; [
@@ -47,16 +28,5 @@
     (zoom-us.override { hyprlandXdgDesktopPortalSupport = true; })
     slack
     devenv
-    obsidian
-    spotify
-    # slurp, grim and satty are required for screenshots
-    slurp
-    grim
-    satty
-    obs-studio
-    cameractrls-gtk4
   ];
-
-  # enable bluetooth by default
-  services.blueman-applet.enable = true;
 }
