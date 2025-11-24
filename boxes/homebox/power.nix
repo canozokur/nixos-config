6e64c08676b4104dde1d1ee9a3dc36b592f9da6d
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-24 02:30:09 +0200

    Move laptop power options to its profile

--- boxes/homebox/power.nix
+++ boxes/homebox/power.nix
@@ -1,10 +1,5 @@
 { ... }:
 {
-  services.upower = {
-    enable = true;
-    criticalPowerAction = "Hibernate";
-  };
-
   # power_save=true actually disables power saving config for some reason
   # and power_level=5 should max it out (hopefully?)
   # for power_scheme explanation see:
--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -4,4 +4,10 @@
   services.logind.settings.Login.HandleLidSwitch = "ignore";
   services.logind.settings.Login.HandleLidSwitchDocked = "ignore";
   services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
+
+  services.upower = {
+    enable = true;
+    criticalPowerAction = "Hibernate";
+  };
+
 }
