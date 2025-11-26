635fa6e16524e9406aed666d97d8c3a893c69b46
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-26 21:22:56 +0200

    Make an exception on aarch for spotify

--- users/canozokur/profiles/desktop.nix
+++ users/canozokur/profiles/desktop.nix
@@ -23,14 +23,13 @@
   ];
   home.packages = with pkgs; [
     obsidian
-    spotify
     # slurp, grim and satty are required for screenshots
     slurp
     grim
     satty
     obs-studio
     cameractrls-gtk4
-  ];
+  ] ++ lib.optionals (system != "aarch64-linux") spotify;
 
   # enable bluetooth by default
   services.blueman-applet.enable = true;
