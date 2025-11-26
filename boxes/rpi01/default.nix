dd1469e396013773834db72682fc4afb1c6ffd24
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-26 12:02:58 +0200

    Add rpi01 box

---
+++ boxes/rpi01/default.nix
@@ -0,0 +1,45 @@
+{ inputs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    inputs.nixos-hardware.nixosModules.raspberry-pi-4
+  ];
+
+  networking = {
+    hostName = "rpi01";
+    nameservers = [
+      "192.168.1.1"
+    ];
+    networkmanager = {
+      enable = true;
+      dns = "none";
+      wifi.powersave = false;
+      ensureProfiles.profiles = {
+        wired = {
+          connection = {
+            id = "wired";
+            permissions = "";
+            type = "802-3-ethernet";
+            interface-name = "eno1";
+          };
+        };
+      };
+    };
+  };
+
+  hardware.graphics.enable = true;
+  # bluetooth config
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.settings.General = {
+    FastConnectable = true;
+    Experimental = true;
+    KernelExperimental = true;
+  };
+  hardware.bluetooth.powerOnBoot = true;
+
+  security.pam.loginLimits = [
+    { domain = "@users"; item = "rtprio"; type = "-"; value = 1; }
+  ];
+
+  system.stateVersion = "25.05";
+}
