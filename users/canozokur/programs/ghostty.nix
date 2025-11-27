bde4c0457262e93b781503732b19a7a3ccf7d704
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-27 20:01:06 +0200

    Fix updated ghostty options

--- users/canozokur/programs/ghostty.nix
+++ users/canozokur/programs/ghostty.nix
@@ -8,14 +8,13 @@
       font-size = 12;
       window-padding-x = 0;
       window-padding-y = 0;
-      theme = "catppuccin-mocha";
+      theme = "Catppuccin Mocha";
       window-decoration = "server";
       alpha-blending = "native";
       minimum-contrast = 1.1;
       background-opacity = 0.97;
       bold-is-bright = true;
       confirm-close-surface = false;
-      gtk-adwaita = false;
       resize-overlay = "never";
       keybind = [
         "ctrl+shift+v=paste_from_clipboard"
