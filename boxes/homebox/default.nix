b59bdd58b0da3eca9d48181853400ab5bc66ea65
canozokur <13416785+canozokur@users.noreply.github.com>
2025-11-29 02:15:07 +0200

    Move network config from common profile to boxes

--- boxes/homebox/default.nix
+++ boxes/homebox/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, inputs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -13,7 +13,7 @@
     };
   };
 
-  networking = {
+   networking = {
     hostName = "homebox";
     nameservers = [
       "192.168.1.1"
@@ -22,13 +22,91 @@
       enable = true;
       dns = "none";
       wifi.powersave = false;
-      ensureProfiles.profiles = {
-        wired = {
-          connection = {
-            id = "wired";
-            permissions = "";
-            type = "802-3-ethernet";
-            interface-name = "eno1";
+      ensureProfiles = {
+        secrets.entries = [
+          {
+            file = config.sops.secrets."network/secrets/home-wifi-5g/psk".path;
+            key = "psk";
+            matchId = "home-wifi-5g";
+            matchSetting = "802-11-wireless-security";
+            matchType = "802-11-wireless";
+          }
+          {
+            file = config.sops.secrets."network/secrets/home-wifi/psk".path;
+            key = "psk";
+            matchId = "home-wifi";
+            matchSetting = "802-11-wireless-security";
+            matchType = "802-11-wireless";
+          }
+        ];
+        profiles = {
+          home-wifi-5g = {
+            connection = {
+              id = "home-wifi-5g";
+              type = "wifi";
+              autoconnect = true;
+            };
+            ipv6 = {
+              addr-gen-mode = "stable-privacy";
+              method = "disabled";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = inputs.nix-secrets.network.home-wifi-5g.ssid;
+            };
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-psk";
+            };
+          };
+          home-wifi = {
+            connection = {
+              id = "home-wifi";
+              type = "wifi";
+              autoconnect = "true";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = inputs.nix-secrets.network.home-wifi.ssid;
+            };
+            wifi-security = {
+              key-mgmt = "wpa-psk";
+            };
+            ipv6 = {
+              addr-gen-mode = "stable-privacy";
+              method = "auto";
+            };
+          };
+          office-wifi-enterprise = {
+            connection = {
+              id = "office-wifi-enterprise";
+              type = "wifi";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = inputs.nix-secrets.network.office-wifi-enterprise.ssid;
+            };
+            wifi-security = {
+              key-mgmt = "wpa-eap";
+            };
+            "802-1x" = {
+              eap = "peap";
+              identity = "dummy";
+              password = "dummy";
+              phase2-auth = "mschapv2";
+            };
+            ipv6 = {
+              addr-gen-mode = "stable-privacy";
+              method = "auto";
+            };
+          };
+          wired = {
+            connection = {
+              id = "wired";
+              permissions = "";
+              type = "802-3-ethernet";
+              interface-name = "eno1";
+            };
           };
         };
       };
