897a17c83cdbae98806054aca9916fcc6f0a3765
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-02 21:54:41 +0200

    Add iscsi-initiator profile

---
+++ profiles/iscsi-initiator.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, ... }:
+{
+  services.openiscsi = {
+    enable = true;
+    discoverPortal = "192.168.0.100:3260";
+    name = lib.mkDefault "iqn.2020-08.org.nixos.${config.system.name}:default";
+    enableAutoLoginOut = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    openiscsi
+  ];
+}
