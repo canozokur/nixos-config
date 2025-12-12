9c7c5216e68d95a8db15cff80b373cd5f970709f
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-12 21:43:58 +0200

    Move _meta option into custom module config

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,29 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./falcon-sensor
   ];
+
+  # Define _meta options
+  options._meta = with lib; {
+    dnsConfiguration = mkOption {
+      description = "List of DNS records associated with this host. These record will be added to pihole as static hosts.";
+      default = [];
+      type = types.listOf (types.submodule {
+        options = {
+          externalIp = mkOption {
+            type = types.str;
+            description = "The external IP address.";
+          };
+          domain = mkOption {
+            type = types.str;
+            description = "The domain name (e.g. 'testing.lan').";
+          };
+        };
+      });
+    };
+
+    # Image payload
+    buildImage = lib.mkEnableOption "This host exports an image to build";
+  };
 }
