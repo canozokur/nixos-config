c8a321ca94fcc144ac06c8d7b1014d6eaa2205a7
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-14 01:14:07 +0200

    Tidy up metadata configuration and add more for ext/int addresses

--- modules/default.nix
+++ modules/default.nix
@@ -6,7 +6,7 @@
 
   # Define _meta options
   options._meta = with lib; {
-    dnsConfiguration = mkOption {
+    dnsConfigurations = mkOption {
       description = "List of DNS records associated with this host. These record will be added to pihole as static hosts.";
       default = [];
       type = types.listOf (types.submodule {
@@ -21,6 +21,17 @@
           };
         };
       });
+    };
+
+    networks = mkOption {
+      description = "External and internal IP addresses of this host";
+      default = {};
+      type = types.submodule {
+        options = {
+          externalIP = with types; mkOption { type = str; default = ""; description = "The primary external address of this host"; };
+          internalIP = with types; mkOption { type = str; default = ""; description = "The primary internal address of this host"; };
+        };
+      };
     };
 
     # Image payload
