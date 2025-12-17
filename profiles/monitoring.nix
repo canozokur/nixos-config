b28fa6a1608801ba624579ffab6976d1f99ec480
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-17 09:35:16 +0200

    Use consul in the monitoring profile and add relabel config

--- profiles/monitoring.nix
+++ profiles/monitoring.nix
@@ -5,10 +5,33 @@
     globalConfig.scrape_interval = "10s";
     scrapeConfigs = [
       {
-        job_name = "node";
-        static_configs = [{
-          targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+        job_name = "consul";
+        consul_sd_configs = [{
+          server = "consul.lan:8500";
         }];
+        # relabel configuration for consul server metrics
+        # documentation: https://developer.hashicorp.com/consul/docs/reference/agent/configuration-file/telemetry#telemetry-prometheus_retention_time
+        relabel_configs = [
+          {
+            source_labels = ["__address__" "__meta_consul_service"];
+            separator = ":";
+            regex = "(.*):(8300):(consul)";
+            target_label = "__address__";
+            replacement = "\${1}:8500";
+          }
+          {
+            source_labels = ["__meta_consul_service"];
+            regex = "consul";
+            target_label = "__param_format";
+            replacement = "prometheus";
+          }
+          {
+            source_labels = ["__meta_consul_service"];
+            regex = "consul";
+            target_label = "__metrics_path__";
+            replacement = "/v1/agent/metrics";
+          }
+        ];
       }
     ];
   };
