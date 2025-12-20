4b44c86877577083874d36d091b8ba65b5ce8767
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-20 15:28:08 +0200

    Make checks follow the exporter port configuration

--- profiles/capabilities/node-exporter.nix
+++ profiles/capabilities/node-exporter.nix
@@ -1,4 +1,7 @@
 { config, ... }:
+let
+  exporterPort = config.services.prometheus.exporters.node.port;
+in
 {
   services.prometheus.exporters = {
     node = {
@@ -13,12 +16,12 @@
     name = "node-exporter";
     tags = ["server"];
     address = config._meta.networks.internalIP;
-    port = config.services.prometheus.exporters.node.port;
+    port = exporterPort;
     checks = [
       {
         id = "node-exporter-check";
-        name = "HTTP on port 9100";
-        http = "http://localhost:9100";
+        name = "HTTP on port ${toString exporterPort}";
+        http = "http://localhost:${toString exporterPort}";
         interval = "10s";
         timeout = "1s";
       }
