28c115406ba23c47183d0c67b1f8cceb4ba837d1
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-23 00:42:45 +0200

    Update reverse-proxy profile and automatically create local dns records

--- profiles/reverse-proxy.nix
+++ profiles/reverse-proxy.nix
@@ -1,7 +1,18 @@
-{ ... }:
+{ helpers, inputs, lib, config, ... }:
+let
+  externalIP = config._meta.networks.externalIP;
+  allVhosts = helpers.getHostsWith inputs.self.nixosConfigurations "externalVhosts";
+  listVhosts = lib.mapAttrsToList (_: host: host.config._meta.externalVhosts) allVhosts;
+  externalVhosts = lib.flatten (lib.map (k: lib.attrNames k) listVhosts);
+in
 {
   imports = [
     ./capabilities/nginx.nix
     ./capabilities/consul.nix
   ];
+
+  _meta.dnsConfigurations = builtins.map (d: {
+    ip = externalIP;
+    domain = d;
+  }) externalVhosts;
 }
