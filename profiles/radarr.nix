cf5e817d66fde4f8b7a9863b4ae54d9ca8cabb34
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-26 22:57:27 +0200

    Create a shared media group

--- profiles/radarr.nix
+++ profiles/radarr.nix
@@ -1,10 +1,10 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   port = config.services.radarr.settings.server.port;
   addr = config._meta.networks.internalIP;
   mountPoint = "/mnt/radarr-data";
   uid = config.ids.uids.radarr;
-  gid = config.ids.gids.radarr;
+  gid = 568;
 in
 {
   imports = [
@@ -29,9 +29,12 @@ in
     };
   };
 
+  users.groups = { media = { inherit gid; }; };
+
   services.radarr = {
     enable = true;
     openFirewall = true;
+    group = "media";
     dataDir = mountPoint;
   };
 
@@ -45,6 +48,13 @@ in
 
   systemd.tmpfiles.rules = [
     # Type Path        Mode    UID                GID             Age  Argument
-    "d ${mountPoint}   0755    ${toString uid}   ${toString gid}  -    -"
+    "d ${mountPoint}   0775    ${toString uid}   ${toString gid}  -    -"
   ];
+
+  # using mkDefault because other profiles might mount the same thing
+  fileSystems."/shared" = lib.mkDefault {
+    device = "192.168.0.100:/mnt/main/k8s/vols/pvc-924a7cdb-6593-4a3b-b498-3fb965cc9ef6";
+    fsType = "nfs";
+    options = [ "nconnect=16" ];
+  };
 }
