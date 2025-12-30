5e88c8318ba5614c6311f2da0c51223919d1f5bc
canozokur <13416785+canozokur@users.noreply.github.com>
2025-12-31 01:59:00 +0200

    Tidy up vhost config

--- profiles/reverse-proxy.nix
+++ profiles/reverse-proxy.nix
@@ -2,7 +2,20 @@
 let
   externalIP = "192.168.1.254";
   internalIP = "192.168.1.253";
-  vhostConfigurations = helpers.getHostsWith inputs.self.nixosConfigurations ["nginx" "vhosts"];
+  serversWithVhosts = helpers.getHostsWith inputs.self.nixosConfigurations ["nginx" "vhosts"];
+  buildDnsCfg = lib.mapAttrsToList (name: node:
+    lib.mapAttrsToList (d: c:
+      let
+        listenList = c.listen or [];
+        customIps = builtins.filter (x: x != null)
+          (builtins.map (e: e.addr or null) listenList);
+        finalIps = if customIps != [] then customIps else [ internalIP ];
+      in
+      builtins.map (ip:
+        { domain = d; ip = ip; }
+      ) finalIps
+    ) node.config._meta.nginx.vhosts
+  );
 in
 {
   imports = [
@@ -11,24 +24,7 @@ in
   ];
 
   _meta = {
-    dnsConfigurations = lib.flatten (
-      lib.mapAttrsToList (name: node:
-        let
-          vhosts = node.config._meta.nginx.vhosts;
-        in
-        lib.mapAttrsToList (d: c:
-          let
-            listenList = c.listen or [];
-            customIps = builtins.filter (x: x != null)
-              (builtins.map (e: e.addr or null) listenList);
-            finalIps = if customIps != [] then customIps else [ internalIP ];
-          in
-          builtins.map (ip:
-            { domain = d; ip = ip; }
-          ) finalIps
-        ) vhosts
-      ) vhostConfigurations
-    );
+    dnsConfigurations = lib.flatten (buildDnsCfg serversWithVhosts);
 
     networks.wiredAddresses = [
       externalIP
