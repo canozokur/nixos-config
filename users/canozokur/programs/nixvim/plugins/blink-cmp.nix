999c2afdd87086371d32a45d91f04cc9a6634e58
canozokur <13416785+canozokur@users.noreply.github.com>
2026-02-17 19:28:59 +0200

    Make tab accept in blink

--- users/canozokur/programs/nixvim/plugins/blink-cmp.nix
+++ users/canozokur/programs/nixvim/plugins/blink-cmp.nix
@@ -6,6 +6,11 @@
       settings = {
         keymap = {
           preset = "default";
+          "<Tab>" = [
+            { __raw = "function(cmp) if cmp.snippet_active() then return cmp.accept() else return cmp.select_and_accept() end end"; }
+            "snippet_forward"
+            "fallback"
+          ];
         };
         appearance = {
           use_nvim_cmp_as_default = true;
