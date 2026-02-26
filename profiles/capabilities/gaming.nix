e9a2383a13a8fc73b168d4062508aa302ae3047b
canozokur <13416785+canozokur@users.noreply.github.com>
2026-02-27 01:28:19 +0200

    Enable protontricks

--- profiles/capabilities/gaming.nix
+++ profiles/capabilities/gaming.nix
@@ -6,6 +6,7 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     gamescopeSession.enable = true;
+    protontricks.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
