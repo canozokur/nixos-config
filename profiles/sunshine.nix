94cd705a5273aa46f0b30686cb350b02660fcbf1
canozokur <13416785+canozokur@users.noreply.github.com>
2026-02-27 10:12:52 +0200

    Add workaround for sunshine build failures

--- profiles/sunshine.nix
+++ profiles/sunshine.nix
@@ -1,9 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.sunshine = {
     enable = true;
     autoStart = true;
     capSysAdmin = false;
     openFirewall = true;
+    package = pkgs.sunshine.override {
+      boost = pkgs.boost187;
+    };
   };
 }
