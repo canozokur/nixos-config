ae38bdb23c257303d697c547cd9a125f5f9e0db3
canozokur <13416785+canozokur@users.noreply.github.com>
2026-02-27 21:11:30 +0200

    Move OS flake options to separate file

---
+++ modules/host-options.nix
@@ -0,0 +1,143 @@
+{ lib, ... }:
+{
+  # Define _meta options
+  options._meta = with lib; {
+    dnsConfigurations = mkOption {
+      description = "List of DNS records associated with this host. These record will be added to pihole as static hosts.";
+      default = [ ];
+      type = types.listOf (
+        types.submodule {
+          options = {
+            ip = mkOption {
+              type = types.str;
+              description = "The external IP address.";
+            };
+            domain = mkOption {
+              type = types.str;
+              description = "The domain name (e.g. 'testing.lan').";
+            };
+          };
+        }
+      );
+    };
+
+    sunshine = {
+      virtualOutput = mkOption {
+        description = "Free GPU port to create virtual output";
+        default = "";
+        type = types.str;
+      };
+    };
+
+    nginx = {
+      vhosts = mkOption {
+        description = "Nginx vhosts that will be configured on the reverse proxy";
+        default = { };
+        type = types.attrs;
+      };
+
+      upstreams = mkOption {
+        description = "Nginx upstreams";
+        default = { };
+        type = types.attrs;
+      };
+    };
+
+    networks = mkOption {
+      description = "External and internal IP addresses of this host";
+      default = { };
+      type = types.submodule {
+        options = {
+          externalIP =
+            with types;
+            mkOption {
+              type = str;
+              default = "";
+              description = "The primary external address of this host";
+            };
+          internalIP =
+            with types;
+            mkOption {
+              type = str;
+              default = "";
+              description = "The primary internal address of this host";
+            };
+          internalInterface =
+            with types;
+            mkOption {
+              type = str;
+              default = "";
+              description = "The primary internal network interface of this host";
+            };
+          wiredAddresses =
+            with types;
+            mkOption {
+              type = listOf str;
+              default = [ ];
+              description = "List of static IPs (i.e. 192.168.1.100/24) for the wired profile";
+            };
+        };
+      };
+    };
+
+    services = {
+      consulServer = lib.mkEnableOption "This host is a Consul Server";
+      dnsServer = lib.mkEnableOption "This host is a DNS server";
+      dhcpServer = lib.mkEnableOption "This host is a DHCP server";
+      galera.clusterName = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "A unique Galera cluster name.";
+      };
+      mysql.instanceName = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "Instance name of this MariaDB standalone server.";
+      };
+      elb = lib.mkEnableOption "This host is host to an nginx external load balancer.";
+    };
+
+    # Image payload
+    buildImage = lib.mkEnableOption "This host exports an image to build";
+
+    desktop = mkOption {
+      description = "Desktop related settings";
+      default = { };
+      type = types.submodule {
+        options = {
+          hyprlandGPU = mkOption {
+            type = types.listOf types.str;
+            default = [ ];
+            description = "Environment variables for Hyprland GPU settings";
+          };
+          waybarTemperaturePath = mkOption {
+            type = types.str;
+            default = "";
+            description = "HWMon path in /sys/devices for waybar to display the temp";
+          };
+        };
+      };
+    };
+
+    prometheus = mkOption {
+      description = "Consul configuration";
+      default = { };
+      type = types.submodule {
+        options = {
+          enabledCollectors = mkOption {
+            type = types.listOf types.str;
+            default = [ "systemd" ];
+            description = "Enabled node-exporter collectors";
+          };
+        };
+      };
+    };
+  };
+
+  # consul services
+  options.services.consul.agentServices = lib.mkOption {
+    type = lib.types.listOf lib.types.attrs;
+    default = [ ];
+    description = "List of services to register with the local Consul agent.";
+  };
+}
