c813c4a0b937a0bef6bcc363e1d7d350a2c280f9
canozokur <13416785+canozokur@users.noreply.github.com>
2026-03-17 13:50:09 +0200

    Update crowdstrike

--- profiles/capabilities/work.nix
+++ profiles/capabilities/work.nix
@@ -56,7 +56,7 @@ in
 
   falconSensor = rec {
     enable = true;
-    version = "7.30.0-18306";
+    version = "7.33.0-18606";
     arch = "amd64";
     cid = config.sops.secrets."falcon-sensor/cid".path;
     debFile = "${secretsPath}/binary/falcon-sensor_${version}_${arch}.deb";
