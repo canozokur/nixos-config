ae630abebfb2aa8564500db21117a730fcd773e8
canozokur <13416785+canozokur@users.noreply.github.com>
2026-05-20 12:49:51 +0300

    Use autocmd to attach to codecompanion buffers

--- users/canozokur/programs/nixvim/plugins/markview.nix
+++ users/canozokur/programs/nixvim/plugins/markview.nix
@@ -24,6 +24,23 @@
     }
   ];
 
+  autoCmd = [
+    {
+      event = "FileType";
+      pattern = "codecompanion";
+      callback = {
+        __raw = ''
+          function(args)
+            local ok, actions = pcall(require, "markview.actions")
+            if ok then
+              actions.attach(args.buf)
+            end
+          end
+        '';
+      };
+    }
+  ];
+
   # see:
   # https://github.com/nix-community/nixvim/issues/3843#issuecomment-3435139071
   performance.combinePlugins.standalonePlugins = [
