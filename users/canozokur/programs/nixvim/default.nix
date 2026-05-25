1ce49c5f3e3f01f63ac01ed33fd1bc2ff9add557
canozokur <13416785+canozokur@users.noreply.github.com>
2026-05-25 18:13:19 +0300

    Move configuration inside right brackets

--- users/canozokur/programs/nixvim/default.nix
+++ users/canozokur/programs/nixvim/default.nix
@@ -9,11 +9,12 @@
       ./nixvim.nix
       ./common-options.nix
     ];
+
+    enable = true;
+    vimdiffAlias = true;
+    defaultEditor = true;
+    # nixvim started using its own nixpkgs, we need to set its config here
+    nixpkgs.config.allowUnfree = true;
   };
 
-  enable = true;
-  vimdiffAlias = true;
-  defaultEditor = true;
-  # nixvim started using its own nixpkgs, we need to set its config here
-  nixpkgs.config.allowUnfree = true;
 }
