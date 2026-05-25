3a989aa0ae1c35696679cb4f72cfe3ee0f7d447c
canozokur <13416785+canozokur@users.noreply.github.com>
2026-05-25 18:20:01 +0300

    Limit grammars to a smaller size

--- users/canozokur/programs/nixvim/plugins/treesitter.nix
+++ users/canozokur/programs/nixvim/plugins/treesitter.nix
@@ -1,8 +1,42 @@
-{ ... }:
+{ pkgs, ... }:
 {
   plugins = {
     treesitter = {
       enable = true;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        c
+        cpp
+        go
+        helm
+        javascript
+        jsdoc
+        lua
+        luadoc
+        nix
+        python
+        tsx
+        typescript
+        yaml
+
+        diff
+        dockerfile
+        gitcommit
+        gitignore
+        git_config
+        git_rebase
+        gomod
+        gosum
+        gowork
+        json
+        markdown
+        markdown_inline
+        query
+        regex
+        toml
+        vim
+        vimdoc
+      ];
       settings = {
         auto_install = false;
         highlight = {
