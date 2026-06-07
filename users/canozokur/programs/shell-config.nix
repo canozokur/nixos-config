36255672da24a19efe79c6994a7bc63511646dfc
canozokur <13416785+canozokur@users.noreply.github.com>
2026-06-07 13:09:52 +0300

    Add git worktree and bare cloning helper functions

--- users/canozokur/programs/shell-config.nix
+++ users/canozokur/programs/shell-config.nix
@@ -1,4 +1,10 @@
-{ pkgs, config, lib, inputs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  inputs,
+  ...
+}:
 {
   # we need this for alias completion in bash
   home.packages = with pkgs; [
@@ -44,29 +50,67 @@
         ''}
       '';
       bashrcExtra = ''
-        # base64 decode helper
-        b64d() {
-          local secret="$1"
-          echo -n "''${secret}" | base64 -d
-        }
-        # git clone helper
-        gitclone(){ pushd ~/data/git; git clone "$@" "$(echo "$@" | sed -E 's#^.*([:/])([^/]+/[^/]+)$#\2#' | sed -E 's#\.git$##')"; popd;}
-        # set google project
-        function setgp() {
-          export CLOUDSDK_CORE_PROJECT=$1
-        }
-        function setgp() {
-          export CLOUDSDK_CORE_PROJECT=$1
-        }
-        # set aws project
-        function setp() {
-          export AWS_PROFILE=$1
-        }
-        function unsetp() {
-          unset AWS_PROFILE
-        }
+            # base64 decode helper
+            b64d() {
+              local secret="$1"
+              echo -n "''${secret}" | base64 -d
+            }
+            # git clone helper
+            function gitclone() {
+              local REPOID="$(echo "$@" | sed -E 's#^.*([:/])([^/]+/[^/]+)$#\2#' | sed -E 's#\.git$##')"
+              mkdir -p ~/data/git
+              pushd ~/data/git
+              git clone "$@" "$REPOID"
+              popd
+            }
+            # git clone helper with worktrees
+            function gitbare() {
+              local REPOID="$(echo "$@" | sed -E 's#^.*([:/])([^/]+/[^/]+)$#\2#' | sed -E 's#\.git$##')"
+              mkdir -p ~/data/git-bare
+              pushd ~/data/git-bare
+              mkdir $REPOID
+              cd $REPOID
+              git clone --bare "$@"
+              popd
+            }
+            # git worktree helper
+            werk() {
+              local cmd="$1"
+              if [[ -z "$cmd" ]]; then
+                git worktree
+                return
+              fi
+              shift
+              if [[ "$cmd" == "add" ]]; then
+                local branch="$1"
+                if [[ -z "$branch" ]]; then
+                  echo "Error: You must provide a branch name."
+                  echo "Usage: werk add <branch>"
+                  return 1
+                fi
+                shift
+                git worktree add "../$branch" "$branch" "$@"
+                cd "../$branch"
+              else
+                git worktree "$cmd" "$@"
+              fi
+            }
+            # set google project
+            function setgp() {
+              export CLOUDSDK_CORE_PROJECT=$1
+            }
+            function setgp() {
+              export CLOUDSDK_CORE_PROJECT=$1
+            }
+            # set aws project
+            function setp() {
+              export AWS_PROFILE=$1
+            }
+            function unsetp() {
+              unset AWS_PROFILE
+            }
 
-        export VAULT_ADDR=${inputs.nix-secrets.bash_env.vault_addr}
+            export VAULT_ADDR=${inputs.nix-secrets.bash_env.vault_addr}
       '';
     };
 
