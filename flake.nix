3d3554f31ae1b28185014d1feb58bfa086106c7c
canozokur <13416785+canozokur@users.noreply.github.com>
2026-08-11 14:22:00 +0300

    nixos-hardware adds nixpkgs input, make it follow the existing one

--- flake.nix
+++ flake.nix
@@ -30,7 +30,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     zjstatus = {
       url = "github:dj95/zjstatus";
