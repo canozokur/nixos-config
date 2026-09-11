ba897cc9987047677d76dd24edd925d54ece8df7
canozokur <13416785+canozokur@users.noreply.github.com>
2026-09-11 22:02:06 +0300

    Fix rpi zfs warnings

--- services/pi.nix
+++ services/pi.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     ./base/remote-builder-client.nix
@@ -7,4 +7,9 @@
 
   # see: https://discourse.nixos.org/t/cannot-build-raspberry-pi-sdimage-module-dw-hdmi-not-found/71804
   boot.initrd.allowMissingModules = true;
+
+  # The sd-image installer profile enables zfs support (with a default hostId)
+  # for every image; the rpis boot ext4, so drop it. Left on, the zfs module
+  # also warns about boot.zfs.forceImportRoot using its default.
+  boot.supportedFilesystems.zfs = lib.mkForce false;
 }
